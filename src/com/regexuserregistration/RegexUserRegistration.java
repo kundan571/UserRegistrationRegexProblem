03b03fdf4d1d4d529356f12aee310449ba2ed158
kundan571 <kundanchauhan1997@gmail.com>
2023-03-02 01:14:37 +0530

    Added: Validate First Name must be start with captial letter and minimum three character

--- src/com/regexuserregistration/RegexUserRegistration.java
+++ src/com/regexuserregistration/RegexUserRegistration.java
@@ -1,7 +1,26 @@
 package com.regexuserregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexUserRegistration {
     public static void main(String[] args) {
-        System.out.println("Welcome To User Registration Problem: ");
+        // taking user input
+//        Scanner scanner = new Scanner(System.in);
+//       String firstName = scanner.nextLine();
+//       String regex = "^[A-Z][a-z]{2,30}";
+        Pattern p = Pattern.compile("^[A-Z][a-z]{2,30}");
+        // for taking user input pass regex instead of Kundan
+        Matcher m = p.matcher("Kundan");
+        boolean b = m.matches();
+        System.out.println(b);
+        // for user input
+//        if (firstName.matches(regex)){
+//            System.out.println("Valid name: ");
+//        }
+//        else {
+//            System.out.println("Invalid name: ");
+//        }
     }
 }
