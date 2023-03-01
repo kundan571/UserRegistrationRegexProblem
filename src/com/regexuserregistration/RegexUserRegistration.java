a7ae6bca4810200d1e3726e23d569e4a6b81d808
kundan571 <kundanchauhan1997@gmail.com>
2023-03-02 00:31:16 +0530

    Added: Welcome message

---
+++ src/com/regexuserregistration/RegexUserRegistration.java
@@ -0,0 +1,7 @@
+package com.regexuserregistration;
+
+public class RegexUserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome To User Registration Problem: ");
+    }
+}
