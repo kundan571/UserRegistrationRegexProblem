9cbf3c4eff10bc8840c31389c0374f82a95b0e18
kundan571 <kundanchauhan1997@gmail.com>
2023-03-02 17:57:23 +0530

    Added: Validate Password At Least One Upper Case Letter

--- src/com/regexuserregistration/RegexUserRegistration.java
+++ src/com/regexuserregistration/RegexUserRegistration.java
@@ -5,14 +5,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexUserRegistration {
-    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String password = scanner.nextLine();
-        String regex = "[a-zA-Z0-9!@#$%^&*]{8,15}";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(password);
-        boolean b = m.matches();
-        System.out.println(b);
+        public void checkUppercase() {
+
+            Scanner scanner = new Scanner(System.in);
+            String password = scanner.nextLine();
+            String regex = "^[a-z0-9!@#$%^&*(?=.*[A-Z])].{8,15}";
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(password);
+            boolean b = m.matches();
+            System.out.println(b);
+        }
+    public static void main(String[] args) {
+            RegexUserRegistration userRegistration = new RegexUserRegistration();
+            userRegistration.checkUppercase();
+
     }
 }
