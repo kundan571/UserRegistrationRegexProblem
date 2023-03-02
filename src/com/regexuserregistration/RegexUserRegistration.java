dd39333e0a7a1fa847dad159f7f2b33f3e4132fb
kundan571 <kundanchauhan1997@gmail.com>
2023-03-02 16:21:36 +0530

    Added: Validate Emails

--- src/com/regexuserregistration/RegexUserRegistration.java
+++ src/com/regexuserregistration/RegexUserRegistration.java
@@ -7,13 +7,12 @@ import java.util.regex.Pattern;
 public class RegexUserRegistration {
     public static void main(String[] args) {
         // taking user input
-//       Scanner scanner = new Scanner(System.in);
-//       String firstName = scanner.nextLine();
-//        String lastName = scanner.nextLIne
-//       String regex = "^[A-Z][a-z]{2,30}";
-        Pattern p = Pattern.compile("^[A-Z][a-z]{2,30}");
+        Scanner scanner = new Scanner(System.in);
+        String emails = scanner.nextLine();
+        String regex = "^[a-zA-z0-9+.+a-z]+@+[bl+.+co+.+in]{2,30}";
+        Pattern p = Pattern.compile(regex);
         // for taking user input pass regex instead of Kundan
-        Matcher m = p.matcher("Kumar");
+        Matcher m = p.matcher(emails);
         boolean b = m.matches();
         System.out.println(b);
         // for user input
